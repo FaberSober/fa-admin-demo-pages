b87a5433f286b2b1bb44721035bc21c48cf6c574
xupengfei <faberxu@gmail.com>
2023-02-02 09:50:26 +0800

    feat: demo-chart-echarts

---
+++ pages/admin/demo/chart/echartsDemo.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { Card } from "antd";
+import * as echarts from 'echarts';
+
+
+/**
+ * @author xu.pengfei
+ * @date 2023/2/2 09:36
+ */
+export default function echartsDemo() {
+
+  useEffect(() => {
+    // 基于准备好的dom，初始化echarts实例
+    const myChart = echarts.init(document.getElementById('echarts-demo1')!);
+    // 绘制图表
+    myChart.setOption({
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [
+        {
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }
+      ]
+    });
+  }, [])
+
+  return (
+    <div className="fa-full-content fa-p12">
+      <Card title="ECharts 入门示例" className="fa-mb12">
+        <div id="echarts-demo1" style={{width: 500, height: 300}} />
+      </Card>
+
+    </div>
+  )
+}
