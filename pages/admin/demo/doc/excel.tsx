dc6798b79377577286ae2c494d65adf42cc1f56c
xu.pengfei <1508075252@qq.com>
2023-03-14 11:02:15 +0800

    feat: base-onlyoffice集成

---
+++ pages/admin/demo/doc/excel.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {OnlyofficeEditor} from "@/components";
+
+/**
+ * @author xu.pengfei
+ * @date 2023/3/13 19:02
+ */
+export default function excel() {
+
+  return (
+    <div className="fa-full-content fa-flex-row fa-p12">
+      <OnlyofficeEditor fileId="c648e3a45224b0805c68f7b0a791e486" />
+    </div>
+  )
+}
