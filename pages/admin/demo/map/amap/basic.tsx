46d0e99740d9b6eb46d87360f5aca29b644a32be
xupengfei <faberxu@gmail.com>
2023-04-21 15:07:39 +0800

    feat: demo-map-amap-basic-地图坐标拾取

--- pages/admin/demo/map/amap/basic.tsx
+++ pages/admin/demo/map/amap/basic.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Map, Marker} from '@uiw/react-amap';
-import {Button, Card, Space} from "antd";
+import {Card} from "antd";
+import AMapAutoComplete from "@features/fa-admin-pages/components/map/AMapAutoComplete";
 
 
 /**
@@ -14,7 +15,7 @@ export default function basic() {
   return (
     <div className="fa-full-content fa-bg-white fa-p12">
       <Card title="基础地图展示" className="fa-mb12">
-        <Map style={{height: 200}} />
+        <Map style={{height: 200}}/>
       </Card>
 
       <Card title="地图坐标拾取" className="fa-mb12">
@@ -25,11 +26,15 @@ export default function basic() {
             setPos([event.lnglat.lng, event.lnglat.lat])
           }}
         >
-          {pos && <Marker position={new AMap.LngLat(pos[0],pos[1])} />}
+          {pos && <Marker position={new AMap.LngLat(pos[0], pos[1])}/>}
         </Map>
         <div>
           点击拾取坐标：lng: {pos && pos[0]}, lat: {pos && pos[1]}
         </div>
+      </Card>
+
+      <Card title="AutoComplete 输入提示" className="fa-mb12">
+        <AMapAutoComplete/>
       </Card>
     </div>
   )
