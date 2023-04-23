3b6b03e0c1757bc930d954be74dbb74245a0f334
xupengfei <faberxu@gmail.com>
2023-04-23 15:43:32 +0800

    feat: wind-ECharts 折线图

--- pages/admin/demo/chart/echartsDemo.tsx
+++ pages/admin/demo/chart/echartsDemo.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Card} from "antd";
-import {EchartsBar, EchartsBase, EchartsPie} from "@/components";
+import {EchartsBar, EchartsPie} from "@/components";
 import EchartsDynamicDemo from "./cube/EchartsDynamicDemo";
 import EchartsBasicDemo from "./cube/EchartsBasicDemo";
+import EchartsSimpleDemo from "@features/fa-admin-demo-pages/pages/admin/demo/chart/cube/EchartsSimpleDemo";
 
 
 /**
@@ -51,29 +52,7 @@ export default function echartsDemo() {
       </Card>
 
       <Card title="ECharts 折线图" className="fa-mb12">
-        <EchartsBase
-          option={{
-            grid: {top: 8, right: 8, bottom: 24, left: 36},
-            xAxis: {
-              type: 'category',
-              data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            },
-            yAxis: {
-              type: 'value',
-            },
-            series: [
-              {
-                data: [820, 932, 901, 934, 1290, 1330, 1320],
-                type: 'line',
-                smooth: true,
-              },
-            ],
-            tooltip: {
-              trigger: 'axis',
-            },
-          }}
-          style={{width: 500, height: 300}}
-        />
+        <EchartsSimpleDemo />
       </Card>
 
       <Card title="ECharts 动态表格" className="fa-mb12">
