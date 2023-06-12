10af4a90cd3e0136513b0f5ea52eb92d6cc263ab
xupengfei <faberxu@gmail.com>
2023-06-12 14:33:47 +0800

    feat: base-代码整理

--- pages/admin/demo/advance/socket/cube/SocketTask.tsx
+++ pages/admin/demo/advance/socket/cube/SocketTask.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Space} from "antd";
+
 
 /**
  * @author xu.pengfei
  * @date 2023/5/29 11:32
  */
 export default function SocketTask() {
+  const [data, setData] = useState()
+
+  function start() {
+
+  }
+
+  function pause() {
+
+  }
+
+  function stop() {
+
+  }
+
   return (
     <div>
       <Space className="fa-mb12">
-        <Button>开启任务</Button>
-        <Button>暂停任务</Button>
-        <Button>停止任务</Button>
+        <Button onClick={start}>开启任务</Button>
+        <Button onClick={pause}>暂停任务</Button>
+        <Button onClick={stop}>停止任务</Button>
       </Space>
 
       <div>
