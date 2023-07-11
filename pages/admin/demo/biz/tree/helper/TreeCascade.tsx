8ab92ba13930d793d1a96c353e50f840a9684b4a
xupengfei <faberxu@gmail.com>
2023-07-11 17:18:06 +0800

    a

--- pages/admin/demo/biz/tree/helper/TreeCascade.tsx
+++ pages/admin/demo/biz/tree/helper/TreeCascade.tsx
@@ -3,12 +3,10 @@ import { BaseCascader, BaseCascaderProps } from '@fa/ui';
 import { treeApi as api } from '@/services';
 import { Demo } from '@/types';
 
-export interface TreeCascadeProps extends Omit<BaseCascaderProps<Demo.Tree>, 'serviceApi'> {}
-
 /**
  * @author xu.pengfei
  * @date 2020/12/25
  */
-export default function TreeCascade(props: TreeCascadeProps) {
+export default function TreeCascade(props: Omit<BaseCascaderProps<Demo.Tree>, 'serviceApi'>) {
   return <BaseCascader showRoot={false} serviceApi={api} {...props} />;
 }
