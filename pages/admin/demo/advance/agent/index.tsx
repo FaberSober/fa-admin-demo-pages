c0b445e82fcd1d1947576f38f76b26e2fe34ee86
xu.pengfei <1508075252@qq.com>
2023-07-15 17:10:43 +0800

    feat: base-代码整理

---
+++ pages/admin/demo/advance/agent/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Card} from "antd";
+
+/**
+ * @author xu.pengfei
+ * @date 2023/7/15 16:32
+ */
+export default function index() {
+  return (
+    <div className="fa-full-content fa-bg-white fa-p12 fa-flex-column">
+      <Card title="判断浏览器类型" className="fa-mb12">
+
+      </Card>
+    </div>
+  )
+}
