4323a1b79f24bb297aacd3026e4c0919b1cbcd84
xu.pengfei <1508075252@qq.com>
2023-09-05 20:30:40 +0800

    base-更新用户角色

---
+++ pages/h5/in/demo/userinfo/index.tsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { UserLayoutContext } from "@features/fa-admin-pages/layout";
+
+/**
+ * @author xu.pengfei
+ * @date 2023/9/5 19:34
+ */
+export default function index() {
+  const { user } = useContext(UserLayoutContext)
+  return (
+    <div>
+      <div>username: {user.name}</div>
+    </div>
+  )
+}
