6c9b349c5b07b1a6d027b78f1412e274d0d3495b
xupengfei <faberxu@gmail.com>
2024-11-24 14:12:42 +0800

    demo-add filePreview page

--- pages/admin/demo/biz/filePreview.tsx
+++ pages/admin/demo/biz/filePreview.tsx
@@ -1,8 +1,7 @@
-import React, { useContext, useState } from 'react';
-import { ConfigLayoutContext, UserLayoutContext } from "@features/fa-admin-pages/layout";
+import React, { useState } from 'react';
 import { Space } from "antd";
-import { FaCipher, FaFlexRestLayout, fileSaveApi, UploadFileLocal } from "@fa/ui";
-import { Admin } from "@features/fa-admin-pages/types";
+import { FaFlexRestLayout, UploadFileLocal } from "@fa/ui";
+import FaFileView from "@features/fa-admin-pages/components/file/FaFileView";
 
 
 /**
@@ -11,32 +10,16 @@ import { Admin } from "@features/fa-admin-pages/types";
  * @date 2024/11/24 10:48
  */
 export default function FilePreview() {
-  const {systemConfig} = useContext(ConfigLayoutContext)
-  const {user} = useContext(UserLayoutContext)
-  const [file, setFile] = useState<Admin.FileSave>();
-
-  let previewUrl = undefined;
-  if (file) {
-    const url = window.location.origin + fileSaveApi.genLocalGetFile(file.id) + '?fullfilename=' + file.originalFilename  + '&watermarkTxt=' + encodeURIComponent(user.name + '/' + user.username)
-    console.log('url', url)
-    previewUrl = systemConfig.kkFileViewUrl + '/onlinePreview?url=' + encodeURIComponent(FaCipher.encryptByBase64(url));
-  }
+  const [fileId, setFileId] = useState<string>();
 
   return (
     <div className="fa-full-content-p12 fa-flex-column">
       <Space className="fa-mb12">
-        <UploadFileLocal
-          // value={fileId}
-          onChange={(v) => {
-            if (v) {
-              fileSaveApi.getById(v).then(res => setFile(res.data))
-            }
-          }}
-        />
+        <UploadFileLocal value={fileId} onChange={(v) => setFileId(v)}/>
       </Space>
 
       <FaFlexRestLayout>
-        <iframe src={previewUrl} className="fa-full fa-iframe fa-scroll-hidden" />
+        <FaFileView fileId={fileId}/>
       </FaFlexRestLayout>
     </div>
   )
