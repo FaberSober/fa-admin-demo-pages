420567382c14e95e82e89575e031b671a85148c0
yang.xiaoyu <xiaoyuyang2001@outlook.com>
2025-12-23 17:18:38 +0800

    Datav: 新增环状图demo

--- pages/admin/demo/chart/datavChartsDemo/index.tsx
+++ pages/admin/demo/chart/datavChartsDemo/index.tsx
@@ -1,14 +1,28 @@
 import { Card } from 'antd';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DatavChinaMapDemo from './cube/map/DatavChinaMapDemo';
 import DatavBarChartDemo from './cube/chart/DatavBarChartDemo';
 import DatavLineChartDemo from './cube/chart/DatavLineChartDemo';
+import DatavRingChartDemo from './cube/chart/DatavRingChartDemo';
 
 /**
  * @author xu.pengfei
  * @date 2025-12-17 10:06:08
  */
 export default function DatavChartsDemo() {
+  const [data, setData] = useState<{ name: string, value: number }[]>([]);
+
+  useEffect(() => {
+    const mockData = [
+      { name: '设备1', value: 10 },
+      { name: '设备2', value: 20 },
+      { name: '设备3', value: 30 },
+      { name: '设备4', value: 40 },
+      { name: '设备5', value: 50 },
+    ];
+    setData(mockData);
+  }, []);
+
   return (
     <div className="fa-full-content fa-p12">
       <Card title="柱状图" className="fa-mb12">
@@ -19,6 +33,11 @@ export default function DatavChartsDemo() {
       <Card title="折线图" className="fa-mb12">
         <div style={{ width: 800, height: 300 }}>
           <DatavLineChartDemo />
+        </div>
+      </Card>
+      <Card title="环状图" className="fa-mb12">
+        <div style={{ width: 800, height: 300 }}>
+          <DatavRingChartDemo data={data} />
         </div>
       </Card>
       <Card title="地图" className="fa-mb12">
