e3da2203da41488650cba250103d07be82c4deb4
xu.pengfei <1508075252@qq.com>
2025-12-24 14:32:30 +0800

    add react-map-gl

--- pages/admin/demo/map/mapbox/index.tsx
+++ pages/admin/demo/map/mapbox/index.tsx
@@ -1,6 +1,9 @@
 import { Card } from 'antd';
 import React from 'react';
-import MapBoxBasicDemo from '../amap/basic/cube/MapBoxBasicDemo';
+import MapBoxBasicDemo from './cube/MapBoxBasicDemo';
+import MapBoxControlDemo from './cube/MapBoxControlDemo';
+import MapBoxCustomCameraDemo from './cube/MapBoxCustomCameraDemo';
+import MapBoxLayerDemo from './cube/MapBoxLayerDemo';
 
 /**
  * @author xu.pengfei
@@ -11,6 +14,18 @@ export default function MapBoxDemoPage() {
     <div className="fa-full-content fa-bg-white fa-p12 fa-scroll-auto-y">
       <Card title="基础地图展示" className="fa-mb12">
         <MapBoxBasicDemo />
+      </Card>
+
+      <Card title="受控地图展示" className="fa-mb12">
+        <MapBoxControlDemo />
+      </Card>
+
+      <Card title="地图-Custom Camera Constraints" className="fa-mb12">
+        <MapBoxCustomCameraDemo />
+      </Card>
+
+      <Card title="地图-Adding Custom Data" className="fa-mb12">
+        <MapBoxLayerDemo />
       </Card>
 
     </div>
